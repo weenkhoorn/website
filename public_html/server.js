1dc1c69d6f8de32a6ea2ac88972a52d0567a1095
Wladimir Eenkhoorn <wladimir.eenkhoorn@gmail.com>
2014-07-11 15:39:42 +0200

    So now there's a little simple check to see if you filled in all the forms. My first steps in nodeJS

--- public_html/server.js
+++ public_html/server.js
@@ -3,7 +3,7 @@ var splitter = require('../scripts/splitter.js');
 
 
 var postHTML =
-  '<html><head><title>Post Example</title></head>' +
+  '<html><head><title>Simple Post and Check script</title></head>' +
   '<body>' +
   '<form method="post">' +
   'Your Fist Name: <input name="first_name"><br>' +
@@ -22,32 +22,55 @@ http.createServer(function (req, res) {
  
     if (body != '')
     {
+        var error = [];
         var hash = splitter.formValues(body);
  
          console.log("input1 = " + hash["first_name"]);
          console.log("input2 = " + hash["last_name"]);
 		 // some new lines by W.Eenkhoorn to check if sumbissoin not empty
-		 //let's do a switch 
-		 
-		 switch(hash){
-			 case hash["first_name"]:
-			 	if(hash["first_name"]==''){
-				 	res.writeHead(200);
-				 	res.write('You did not fill in your name');
-				 	return;
-				 	
-			 	} break;
-			 case hash["last_name"]:
-			 	if(hash["last_name"]==''){
-				 	res.writeHead(200);
-				 	res.write('You did not fill in your lastname');
-				 	return;
-				 	
-			 	} break;	
-			 	
+		 // try 1; let's do a switch ; this won't work because a switch won't handle arrays
+		 // so we have to check another way
+		 // let's set an error 
+		 		 
+		 if(Array.isArray(hash)){
+			 //so from here we know it's an array
+			 
+			 if(hash['first_name'] =='' || hash['first_name'] == null){
+				error.push('* Fill in first name <br>');  
+			 }
+			 
+			 if(hash['last_name'] =='' || hash['first_name'] ==null){
+				 error.push('* Fill in last name <br>');
+			 }
+			 
+			 //so now we know that their could be an error let;s do somthing with it!
+			 // we'll create a message
+			 if(error.length > 0 ){
+				var showErrorLines = ''; 
+				for(i=0; i<error.length; i++){
+					showErrorLines += error[i];
+				}				 
+				
+				 //show some html with a back link
+				 var errorHtml = '<html><head><title>Post Example</title></head>' +
+				 '<body>' + showErrorLines +
+				 '<a href="/" >Go Back</a>'+
+				 '</body></html>';
+				 
+			    res.writeHead(200);
+				res.write(errorHtml);
+				res.end();
+				return; 
+			 }
+			 
+		 }else{
+		 	// since we do expect an araay ther must be somthing wrong!!
+			res.writeHead(200);
+			res.write('something went wrong!');
+			res.end(); 
 		 }
 		
-		 
+		 // if everything is oke show names
          res.writeHead(200);
          res.write('Hello ' + hash["first_name"] + ', ' + hash["last_name"] + '!');
          res.end();
